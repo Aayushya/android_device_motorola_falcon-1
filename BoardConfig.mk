9e571a7b1688af8c990efdfb24d01fabc743f3fb
Aayushya Aankul <aayushyaaankul@gmail.com>
2017-01-08 23:37:52 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,8 @@
 
 DEVICE_PATH := device/motorola/falcon
 
+TARGET_KERNEL_APPEND_DTB := true
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsds,falcon_cdma,falcon_retuaws,falcon,falcon_gpe
 
